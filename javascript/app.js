0830645cb507ffa8a6b5113da4911ad3ca234f93
Iremiwa <somoayo@yahoo.com>
2019-10-21 05:59:16 +0100

    Styled Book Cards

--- javascript/app.js
+++ javascript/app.js
@@ -7,7 +7,7 @@ $('#button').click(function fetchBooks(){
 
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=${searchText}&key=AIzaSyAR-RLGGQwIV6RbrR8fRIfWTBZKKvdZRCo`, true, );
+xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=${searchText}&key=AIzaSyAR-RLGGQwIV6RbrR8fRIfWTBZKKvdZRCo&maxResults=12`, true, );
 
 
 
@@ -23,17 +23,32 @@ xhr.onload= function() {
                 console.log("searchResults", $('.searchResults'));
 
                 const output =
-                 `  <div class="row">
-                    <img src=${bookSearch.volumeInfo.imageLinks.thumbnail} alt="bookcover"> 
-
-                        <div class=" col-sm-4 column">
-                        <p>${bookSearch.volumeInfo.title}</p>
-                        <p>${bookSearch.volumeInfo.description}</p>
-                        <p>${bookSearch.volumeInfo.authors}</p>
-                        <p><input type="button" src="${bookSearch.volumeInfo.title}" value="Preview" class="btn btn-primary"></p>
+                `
+                
+                
+                <div class="card " style="width: 300px; height:800px; 
+                position: relative; float: left; padding: 20px; margin:10px;" >
+                                        
+                        <img class="card-img-top" src=${bookSearch.volumeInfo.imageLinks.thumbnail} alt="bookcover" style="height: 300px;"> 
+              
+                        <div class= "card-body">
+                        <h5 class ="card-title">${bookSearch.volumeInfo.title}</p>
+                        <p class ="card-text">Author(s): ${bookSearch.volumeInfo.authors}</p>
+                        <p class ="card-text"> Category: ${bookSearch.volumeInfo.categories}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Pages: ${bookSearch.volumeInfo.pageCount}</li>
+                        <li class="list-group-item">Publisher: ${bookSearch.volumeInfo.publisher}</li>
+                        <li class="list-group-item">Year: ${bookSearch.volumeInfo.publishedDate}</li>
+                        </ul>
+                        <div class="card-body">
+                        <a href="${bookSearch.volumeInfo.previewLink}" class="btn btn-primary">Preview</a>
                         
                         </div>
+                    
                     </div>
+                    </div>
+                   </div>
                  `;
 
                 $('.searchResults').append(output);
@@ -46,5 +61,5 @@ xhr.onload= function() {
     }
 xhr.send();
 
-
 });
+
